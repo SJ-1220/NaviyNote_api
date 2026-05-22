609739fe48ba043d76f0b7466e3353f195a311ec
SJ-1220 <https://github.com/SJ-1220>
2026-05-22 14:30:43 +0900

    Feat: 유저 인증 및 Middleware 구현

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import { authenticateUser } from "./middleware/authenticateUser.js";
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,15 @@ app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
+});
+
+app.get("/test/middleware", authenticateUser, (req, res) => {
+  const userId = res.locals.userId;
+  console.log(`[인증 성공] 유저 ID 추출 완료: ${userId}`);
+  return res.json({
+    success: true,
+    userId: userId,
+  });
 });
 
 export default app;
